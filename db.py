9ce3c58b0ff002f4a8539c855e10ef16212c1205
shashwat <electramite@gmail.com>
2021-02-23 12:16:52 +0530

    Add files via upload

---
+++ db.py
@@ -0,0 +1,21 @@
+import mysql.connector
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  password="@Godown69",
+  database="CBM"
+)
+mycursor = mydb.cursor()
+#insert
+sql = "INSERT INTO Sensor_data (data, time) VALUES (%s, %s)"
+
+
+val = ("273.00", "12:36")
+mycursor.execute(sql, val)
+mydb.commit()
+print(mycursor.rowcount, "record inserted.")
+
+mycursor.execute("SELECT * FROM Sensor_data")
+myresult = mycursor.fetchall()
+for x in myresult:
+  print(x)
---
+++ esp_data_to_database.py
@@ -0,0 +1,36 @@
+import socket
+import mysql.connector
+import time
+
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  password="@Godown69",
+  database="CBM"
+)
+mycursor = mydb.cursor()
+sql = "INSERT INTO Sensor_data (data, time) VALUES (%s, %s)"
+ 
+s = socket.socket()         
+s.bind(('0.0.0.0', 8020 ))
+s.listen(0)                 
+ 
+while True:
+ 
+    client, addr = s.accept()
+ 
+    while True:
+        content = client.recv(32)
+ 
+        if len(content) ==0:
+           break 
+        else:
+      
+            content = content.decode("utf-8")
+            ti = time.gmtime()
+            val = (f"{content}", f"{time.asctime(ti)}")
+            mycursor.execute(sql, val)
+            mydb.commit()
+            print(mycursor.rowcount, "record inserted.")
+    client.close()
